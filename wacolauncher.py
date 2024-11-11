32905aff1f1a20a2cfee48b694a6b4524d314d06
Homanti <homanti0mb2@gmail.com>
2024-11-11 13:55:47 -0800

    bag fix

--- wacolauncher.py
+++ wacolauncher.py
@@ -578,7 +578,7 @@ class Api:
                         "jvmArguments": [
                             f"-Xmx{settings['ram']}m",
                             f"-Xms{settings['ram']}m",
-                            "-javaagent:D:/python_projects/wacolauncher/authlib-injector-1.2.5.jar=https://wacoapi-production.up.railway.app/"
+                            "-javaagent:D:/python_projects/wacolauncher/authlib-injector-1.2.5.jar=https://wacoapi-production.up.railway.app"
                         ],
                         "uuid": account["result"][8],
                         "token": account["result"][9]
